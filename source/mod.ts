0dc03c81362bac13be6c2fb46f057f38a6bc6b20
4uruanna <adrien@villalonga.software>
2026-06-03 14:50:25 +0200

    release: 0.1.5

--- source/mod.ts
+++ source/mod.ts
@@ -5,6 +5,21 @@
 export { default as SqlQueryBuilder } from "./abstract/SqlQueryBuilder.ts";
 
 /****************************************
+ * Interfaces                           *
+ ****************************************/
+
+export type { default as IDeleteQueryBuilder } from "./interface/IDeleteQueryBuilder.ts";
+export type { default as IInsertQueryBuilder } from "./interface/IInsertQueryBuilder.ts";
+export type { default as ISelectQueryBuilder } from "./interface/ISelectQueryBuilder.ts";
+export type { default as IUpdateQueryBuilder } from "./interface/IUpdateQueryBuilder.ts";
+
+export type { default as IBind } from "./interface/IBind.ts";
+export type { default as IBuild } from "./interface/IBuild.ts";
+export type { default as IJoin } from "./interface/IJoin.ts";
+export type { default as IQuery } from "./interface/IQuery.ts";
+export type { default as IWhere } from "./interface/IWhere.ts";
+
+/****************************************
  * Enumerator                           *
  ****************************************/
 
